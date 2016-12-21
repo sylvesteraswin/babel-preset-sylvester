96056e3dfc0095c0ebb66ef2aca3ad78706fe738
Sylvester Aswin <sylvester.aswinstanley@gmail.com>
2016-12-21 17:27:40 +0100

    Included aslant and updated file as per lint errors.

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-function presets() {
-    return {
-        "presets": [
-            [require('babel-preset-es2015'), { loose: true }], 
-            require('babel-preset-react'), 
-            require('babel-preset-stage-1')
-        ],
-        "plugins": [
-            [
-                require('babel-plugin-transform-react-remove-prop-types'), {
-                    "mode": "wrap"
-                }
-            ],
-            require('babel-plugin-transform-object-assign'),
-            [require('babel-plugin-transform-es2015-template-literals'), { spec: true }],
-            require('babel-plugin-transform-exponentiation-operator'),
-            require('babel-plugin-syntax-trailing-function-commas'),
-            [require('babel-plugin-transform-object-rest-spread'), { useBuiltIns: true }]
-        ],
-        "env": {
-            "start": {
-                "presets": [require('babel-preset-react-hmre')]
+module.exports = {
+    presets: [
+        [require('babel-preset-es2015'), {
+            loose: true
+        }],
+        require('babel-preset-react'),
+        require('babel-preset-stage-1')
+    ],
+    plugins: [
+        [
+            require('babel-plugin-transform-react-remove-prop-types'), {
+                mode: 'wrap'
             }
+        ],
+        require('babel-plugin-transform-object-assign'),
+        [require('babel-plugin-transform-es2015-template-literals'), {
+            spec: true
+        }],
+        require('babel-plugin-transform-exponentiation-operator'),
+        require('babel-plugin-syntax-trailing-function-commas'),
+        [require('babel-plugin-transform-object-rest-spread'), {
+            useBuiltIns: true
+        }]
+    ],
+    env: {
+        start: {
+            presets: [require('babel-preset-react-hmre')]
         }
-    };
-}
-
-module.exports = presets();
+    }
+};
